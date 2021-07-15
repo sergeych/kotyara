3ff178573fbd4e9d8195a3c901b201dfcf33be47
sergeych <real.sergeych@gmail.com>
2021-07-15 18:41:48 +0300

    added parse ISO8601 date only, uses beginning of day in UTC zone

--- src/test/kotlin/net/sergeych/tools/Iso8601KtTest.kt
+++ src/test/kotlin/net/sergeych/tools/Iso8601KtTest.kt
@@ -12,7 +12,7 @@ internal class Iso8601KtTest {
 //        val x = ZonedDateTime.now().iso8601.iso8601ToZonedDateTime()
         println(ZonedDateTime.now().iso8601)
         val x = "2017-07-24T12:01:00Z".iso8601ToZonedDateTime()
-        val y = "2021-07-15T15:30:31Z".iso8601ToZonedDateTime()
+//        val y = "2021-07-15T15:30:31Z".iso8601ToZonedDateTime()
         println(x)
 //        assertEquals(1, x)
     }
