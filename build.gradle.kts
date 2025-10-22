216fc4f9a84dfdda4736c7bdbb1d8f9abe797e24
sergeych <real.sergeych@gmail.com>
2025-10-22 22:49:52 +0200

    migration to Kotlin 2.2.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,18 +1,19 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 //import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.1.0"
-    kotlin("plugin.serialization") version "2.0.0"
+    kotlin("jvm") version "2.2.20"
+    kotlin("plugin.serialization") version "2.2.20"
     id("java-library")
     id("org.jetbrains.dokka") version "2.0.0"
     `maven-publish`
 }
 
 group = "net.sergeych"
-version = "1.6.2"
+version = "1.7.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
@@ -24,11 +25,11 @@ repositories {
 dependencies {
     implementation(kotlin("reflect"))
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.1")
-    api("net.sergeych:mp_stools:[1.3.3,)")
+    api("net.sergeych:mp_stools:[1.6.2,)")
     implementation("net.sergeych:boss-serialization-mp:0.2.10")
-    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
-    api("net.sergeych:mp_bintools:0.1.9-SNAPSHOT")
-    implementation("com.ionspin.kotlin:bignum:0.3.8")
+    implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
+    api("net.sergeych:mp_bintools:0.2.2")
+    implementation("com.ionspin.kotlin:bignum:0.3.10")
     testImplementation(kotlin("test"))
     testImplementation("org.postgresql:postgresql:42.5.1")
     testImplementation("com.h2database:h2:2.2.220")
@@ -43,8 +44,10 @@ tasks.test {
     useJUnitPlatform()
 }
 
-tasks.withType<KotlinCompile>() {
-    kotlinOptions.jvmTarget = "1.8"
+kotlin {
+    compilerOptions {
+        jvmTarget.set(JvmTarget.JVM_11)
+    }
 }
 
 tasks.named<KotlinCompilationTask<*>>("compileKotlin").configure {
@@ -53,14 +56,8 @@ tasks.named<KotlinCompilationTask<*>>("compileKotlin").configure {
     compilerOptions.freeCompilerArgs.add("-opt-in=kotlin.ExperimentalUnsignedTypes")
 }
 val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
 
 val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions {
-    jvmTarget = "1.8"
-}
 
 tasks.jar {
     manifest {
